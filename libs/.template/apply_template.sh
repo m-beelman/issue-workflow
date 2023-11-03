cdbbfc0f8b101cb47717407d0c45b3a91a5c6e1f
Beelmann, Marko <marko.beelmann@philips.com>
2023-11-03 23:02:21 +0100

    Update

--- libs/.template/apply_template.sh
+++ libs/.template/apply_template.sh
@@ -10,31 +10,38 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
+#
+# get the "Project name" value for following json '{"Project Name":"user-lib"} with jq and store it in the variable "project_name"
+#
+project_name=$(cat $1 | jq -r '.["Project Name"]')
+
+echo "Project name: $project_name"
+
 # Check if the project name is valid
-if [[ ! $1 =~ ^[a-zA-Z0-9_]+$ ]]
+if [[ ! $project_name =~ ^[a-zA-Z0-9_\-]+$ ]]
   then
     echo "Invalid project name"
     exit 1
 fi
 
 # Check if the project already exists
-if [ -d "libs/$1" ]
+if [ -d "libs/$project_name" ]
   then
     echo "Project already exists"
     exit 1
 fi
 
 # Create the project folder
-mkdir libs/$1
+mkdir libs/$project_name
 
 # Copy the template files into the project folder except the apply_template.sh file
-cp -r libs/.template/* libs/$1
-rm libs/$1/apply_template.sh
+cp -r libs/.template/* libs/$project_name
+rm libs/$project_name/apply_template.sh
 
 
 # Replace the project name in the CMakeLists.txt file
-sed -i "s/PROJECT_NAME_REPLACE/$1/g" libs/$1/CMakeLists.txt
+sed -i "s/PROJECT_NAME_REPLACE/$project_name/g" libs/$project_name/CMakeLists.txt
 
 # add the project to the top level CMakeLists.txt (add_subdirectory)
-echo "add_subdirectory(libs/$1)" >> CMakeLists.txt
+echo "add_subdirectory(libs/$project_name)" >> CMakeLists.txt
 
